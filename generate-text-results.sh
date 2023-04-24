4053d7e09d47466520a767d3dee02ce2e201218f
Lucian Popescu <lucian.popescu187@gmail.com>
2023-04-24 20:48:55 +0300

    Delete results dir before generating text results

--- generate-text-results.sh
+++ generate-text-results.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 mkdir text-results/ || true
+rm -rf text-results/*
+rm -rf /var/lib/phoronix-test-suite/test-results/*
 cp -r results/* /var/lib/phoronix-test-suite/test-results/
 
 for p in $(ls -1 /git/ub-benchmark-infra/results)
@@ -8,10 +10,10 @@ do
 	echo y | php /git/phoronix-test-suite/pts-core/phoronix-test-suite.php show-result $p/ | tee text-results/$p.txt
 done
 
-cd text-results/
-for res in $(ls -1 ./)
-do
-	sed -i '1,7d' $res
-	sed -i '4,36d' $res
-	sed -i "1s/.*/hardware info:/" $res
-done
+#cd text-results/
+#for res in $(ls -1 ./)
+#do
+#	sed -i '1,7d' $res
+#	sed -i '4,36d' $res
+#	sed -i "1s/.*/hardware info:/" $res
+#done
