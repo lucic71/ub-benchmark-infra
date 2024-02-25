6ddf2dc83405c03f8a8dc4953ba5272b32a76902
Lucian Popescu <lucian.popescu187@gmail.com>
2024-02-25 14:18:07 +0200

    Add prepare env script

---
+++ prepare-benchmark-env.sh
@@ -0,0 +1,6 @@
+# Disable turbo boost
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+# Disable hyperthreading
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+# Put CPUs in max frequency with performance governor
+sudo cpupower frequency-set -g performance --min `cpupower frequency-info | grep "hardware limits" | awk '{print $6,$7}' | tr -d ' '` --max `cpupower frequency-info | grep "hardware limits" | awk '{print $6,$7}' | tr -d ' '`
