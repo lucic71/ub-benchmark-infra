5afa796e98f4fed8a6f31f1191e13764b77ba4e4
Lucian Popescu <lucian.popescu187@gmail.com>
2024-07-09 06:59:18 +0000

    Add rest of the packages

--- deps.sh
+++ deps.sh
@@ -1,4 +1,11 @@
-# TODO: cpupower
-for p in php g++ binutils-dev libhwy-dev; do
-		sudo apt install -y $p
+#!/bin/bash
+
+for p in php g++ binutils-dev libhwy-dev linux-tools-$(uname -r); do
+	sudo apt install -y $p || true
 done
+
+if `lscpu | grep -q arm`; then
+	for p in linux-modules-extra-6.5.0-1025-oracle; do
+		sudo apt install -y $p || true
+	done
+fi
