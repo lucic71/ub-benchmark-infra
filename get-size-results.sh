2aa5897d8e485354ae4e8b1a98c86260949604c2
Lucian Popescu <lucian.popescu187@gmail.com>
2025-03-02 22:51:24 +0200

    Small fix

--- get-size-results.sh
+++ get-size-results.sh
@@ -10,7 +10,7 @@ BMSNO=$((`echo $BMS | tr -cd ':' | wc -c`+1))
 
 for i in $(seq 1 $FLAGSNO); do
         flags=`echo $FLAGS | cut -d':' -f$i | tr -d ' '`
-        if [ $flags = "" ]; then flags="-base"; fi
+        if [ "$flags" = "" ]; then flags="-base"; fi
 
         for j in $(seq 1 $BMSNO); do
                 searchStr=$(echo $SEARCHSTR | cut -d':' -f$j | tr -d ' ')
